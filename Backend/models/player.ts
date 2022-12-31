1a0f0a94dff3b0414489c8d8fe5e524024c7130c
karankurbur <karankurbur@gmail.com>
2022-12-31 14:45:53 -0800

    fix folders

---
+++ Backend/models/player.ts
@@ -0,0 +1,15 @@
+export type Player {
+    name: string;
+    hitpoints: number;
+    run: number;
+    x: number;
+    y: number;
+    food: number;
+    energySips: number;
+    mageCasts: number;
+    bindCasts: number;
+    arrows: number;
+    target: string;
+    inZone: string;
+    died: string;
+}
